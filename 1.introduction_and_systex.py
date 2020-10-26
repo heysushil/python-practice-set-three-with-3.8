b7dafad691a9e4b3804d9cf7c2c484f38901564d
heysushil <sushil.chaudhary111@gmail.com>
2020-10-26 19:10:40 +0530

    introduction

--- 1.introduction_and_systex.py
+++ 1.introduction_and_systex.py
@@ -17,3 +17,66 @@ Ye multiline comment hai
 """
 Ye bhi multiline comment hai
 """
+
+
+'''
+Varable works and taks: Term condtions of varaible
+
+1. variable kisi bhi value ko store karta hai
+2. varaibel unique hota hai. means small a aur capital A 2 alag varaibles hain.
+3. varaibel ke name kabhi bhi digit se start nahi hoga: 9a not acceptable
+4. variable me _ undersccore ka use kar sakte hai: _a or a_
+5. varaible me space ya other spaacial character nahi use hote hai.
+
+Data kitne type ke hote hain:
+
+1. Text data: Alphabets, Numbers
+2. Multimedia data: Audio, Video, Image, Doc, PDF
+'''
+
+ab = 10
+print(ab)
+
+a = 3
+b = 5
+asum = a + b
+a_sum = a + b
+a9989 = a + b
+
+message = 'Sum of asum: '
+print('Hello python')
+print(message, asum)
+
+
+# ek sath 3 variable ko values allot karna
+a = 10
+b = 20
+
+# 011a = 10
+# a b = 10
+# a-b = 10
+
+
+'''
+H.M:
+
+1. Find Special chars names: , . : ; ' " ? < > / \ () {} [] ! ~ # & - _ |
+'''
+
+# \n used for new line
+print('\nHello line')
+print('\nHello line')
+print('\nHello line')
+
+# condtion
+name = 'Ram'
+
+if name == 'Ram':
+    print('\n', name)
+    print('\n', name)
+    print('\n', name)
+    print('\n', name)
+    print('\n', name)
+    print('\n', name)
+    print('\n', name)
+

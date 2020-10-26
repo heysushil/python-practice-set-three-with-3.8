432f0149524993b020b915546ec742bb2fe9def3
heysushil <sushil.chaudhary111@gmail.com>
2020-10-26 18:19:28 +0530

    first commit

---
+++ 1.introduction_and_systex.py
@@ -0,0 +1,19 @@
+'''
+Comment type:
+
+1. single line comment: use #
+2. multiline comment: single 3 quotes or double 3 quotes 
+
+Note: Statring me coding ke sath comment jarur use karo
+'''
+
+# Ye single line comment hai.
+# Single line comment sort cute: ctrl + forward slash
+
+'''
+Ye multiline comment hai
+'''
+
+"""
+Ye bhi multiline comment hai
+"""

8dad2b0e18e117326e7458e9e1514613d29b9c4b
heysushil <sushil.chaudhary111@gmail.com>
2020-11-20 19:23:41 +0530

    function file

--- 16.function_exe1.py
+++ 16.function_exe1.py
@@ -26,6 +26,11 @@ pass_list_on_function_as_argument(mylist, [1,2,3,4,5], myinfo)
 
 # print('\nHi, {}'.format(name))
 
+# Ye funciton definition hai
+# Yaha par fucntion ka jo ek work decide kiya gaya hai wo karna hai.
+# like as: hame 2 humber ka sum karna hai to hum ko 2 arguments ki requirment hogi.
+# jiske liye hum fucniton definine karte time 2 arguments hamare function ke ansar pass karenge.
+
 def myfun(mylist):
     print('\nType: ', type(mylist), '\nLen: ', len(mylist), '\nMylist: ', mylist)
     
@@ -36,3 +41,37 @@ mylist = [1,'Hello',3,[4,2,1,3],5,(1,2,3,6)]
 
 result = myfun(mylist)
 print(result)
+
+
+# user define sum funciton
+'''
+Ye funciton 2 numbers ke sum ke liye hai.
+Yaha par mere funcitonko 2 arguemtns num1 aur num2 ki requirments hai
+ye numbers interger number honge ye mujhe dhyan rakhna hai.
+'''
+def mysum_function(num1, num2):
+    '''
+    Yaha par hame num1 aur num2 ki values mil chuki hain.
+    Inko sum karne ke liye ek aur variable ki jarurat hai
+    Iske liye main num3 variable banaungi.
+    Then us variable ke andar num1 aur num2 ka sum karungi.
+    '''
+    num3 = num1 + num2
+
+    '''
+    Ab mujhe num3 vairbale ko print karna hai
+    Taki main num3 ka answer dekh saku
+    '''
+
+    print('\nSum: ', num3)
+
+
+'''
+Ab yaha par jo funciton main define kiya hai usko call karungi.
+Is funciton ko call karne se hi main iske andar ke work aur uska output dekh sakti hun.
+Dhyan rahe agar funciton define karte time main funciton me argumets bhi diye hain. 
+To function ko call karte time utne hi arguents pass karna jaruri hai.
+'''
+num1 = int(input())
+mysum_function(num1, 1000)
+
---
+++ 17.lambda_function.py
@@ -0,0 +1,42 @@
+'''
+Lambda Function ke bare me:
+
+1. Ye ek anonymous function hai.
+2. Lambda function me hum multiple arguemts de sakte hain. But iss fucntion ke andar kewal single expression diya ja sakta hai.
+
+Lambda fucntion Syntax:
+
+lambda numberOfArguments : your expression
+'''
+
+'''
+Lambda fucntion me colon se pahle jo 'message' likha hai wo ek argument hai.
+AUr colon ke bad print jo kiya gaya hai wo ek expression hai ki lambda ko call karte hi wo hame ye output show kardega.
+'''
+
+# ye lambda fucniton ko define kiya gaya
+mylambda = lambda message : print('\n', message)
+
+# call lambda function
+mylambda('Hello Lambda funciton.')
+
+# sum fucntion using lambda
+mysum = lambda a, b : a + b
+# recive_mysum_answer = mysum(1000, 200)
+# mynum = int(input('\nEnter first number: '))
+# print('\nMy sum lambda answer: ', mysum(int(input('\nEnter first number: ')), 200))
+print('\nMy sum lambda answer: ', mysum(100, 200))
+
+# Use lambda funciton in normal funciton
+def my_lam_sum_fun(normal_a_argument):
+    # define lambda fucnton with one argument
+    mysum = lambda lambda_b_argument : normal_a_argument + lambda_b_argument
+    # call lambda function and pass one argument lambda_b_argument
+    output = mysum(75)
+    print('\nMy output: ', output)
+
+# call normal fucton and pass one argumet normal_a_argument
+my_lam_sum_fun(25)
+
+
+
